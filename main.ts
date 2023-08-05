405daa2ca27c678ca114dcc1e1affe7133e51bca
Abdfn <64256993+abdfnx@users.noreply.github.com>
2023-08-05 11:51:15 +0300

    update template

--- main.ts
+++ main.ts
@@ -1,53 +1,30 @@
-import { SlashCommandBuilder, Routes } from "discord.js";
 import { REST } from "@discordjs/rest";
-import { Client, GatewayIntentBits } from "discord.js";
-import { GetToken, GetAppId, GetGuildId } from "botway.js";
+import { WebSocketManager } from "@discordjs/ws";
+import { GatewayDispatchEvents, GatewayIntentBits, Client } from "@discordjs/core";
 
-const commands = [
-  new SlashCommandBuilder()
-    .setName("ping")
-    .setDescription("Replies with pong!"),
-  new SlashCommandBuilder()
-    .setName("server")
-    .setDescription("Replies with server info!"),
-  new SlashCommandBuilder()
-    .setName("user")
-    .setDescription("Replies with user info!"),
-].map((command) => command.toJSON());
+const rest = new REST({ version: "10" }).setToken(process.env.DISCORD_TOKEN);
 
-const rest = new REST({ version: "10" }).setToken(GetToken());
-
-// how I can get the id of my server? the answer in this github discussion https://github.com/abdfnx/botway/discussions/4#discussioncomment-2653737
-rest
-  .put(
-    Routes.applicationGuildCommands(GetAppId(), GetGuildId("YOUR_SERVER_NAME")),
-    { body: commands }
-  )
-  .then(() => console.log("Successfully registered application commands."))
-  .catch(console.error);
-
-const client = new Client({ intents: [GatewayIntentBits.Guilds] });
-
-client.once("ready", () => {
-  console.log("Ready!");
+const gateway = new WebSocketManager({
+  token: process.env.DISCORD_TOKEN,
+  intents: GatewayIntentBits.GuildMessages | GatewayIntentBits.MessageContent,
+  rest,
 });
 
-client.on("interactionCreate", async (interaction: any) => {
-  if (!interaction.isChatInputCommand()) return;
+const client = new Client({ rest, gateway });
 
-  const { commandName } = interaction;
+client.on(
+  GatewayDispatchEvents.MessageCreate,
+  async ({ data: msgData, api }) => {
+    if (msgData.author.bot) {
+      return;
+    }
 
-  if (commandName === "ping") {
-    await interaction.reply("Pong!");
-  } else if (commandName === "server") {
-    await interaction.reply(
-      `Server name: ${interaction.guild.name}\nTotal members: ${interaction.guild.memberCount}`
-    );
-  } else if (commandName === "user") {
-    await interaction.reply(
-      `Your tag: ${interaction.user.tag}\nYour id: ${interaction.user.id}`
-    );
+    if (msgData.content === "ping") {
+      await api.channels.createMessage(msgData.channel_id, { content: "pong !" });
+    }
   }
-});
+);
 
-client.login(GetToken());
+client.once(GatewayDispatchEvents.Ready, () => console.log("Ready!"));
+
+gateway.connect();
